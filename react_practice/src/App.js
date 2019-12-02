a1b7cfd175cda10060c35e78e07e2543daf38df0
thelaurelyy <3246997955@qq.com>
2019-12-02 10:55:35 +0800

    JSX语法简介

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
 
 class App extends Component{
     render() {
         return (
-            <div>
-                Hello React
-            </div>
+            <ul className="my-list">
+                <li>{false? 'JSPang.com' : '技术胖'}</li>
+                <li>I love React</li>
+            </ul>
         )
     }
 }
+
 export default App;
