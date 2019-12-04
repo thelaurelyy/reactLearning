73d3356b1d1f4c2a07ae6f4ee39105e27a6a2b6f
thelaurelyy <3246997955@qq.com>
2019-12-04 14:26:42 +0800

    React数据校验PropTypes：（1）数据类型校验；（2）必填校验；（3）设置默认值defaultProps；

--- react_practice/src/menuItem.js
+++ react_practice/src/menuItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class MenuItem extends Component{
     constructor(props) { // constructor 绑定this方法
@@ -7,7 +8,7 @@ class MenuItem extends Component{
     }
     render() {
         return (
-            <div onClick={this.handleClick}>{this.props.content}</div>
+            <div onClick={this.handleClick}>{this.props.content}：{this.props.price} 元</div>
         )
     }
     handleClick() {
@@ -15,5 +16,17 @@ class MenuItem extends Component{
         this.props.deleteItem(this.props.index)
     }
 }
+
+// MenuItem.PropTypes = {   // 错误写法
+MenuItem.propTypes = {
+    content: PropTypes.string.isRequired,
+    deleteItem: PropTypes.func,
+    index: PropTypes.number,
+    price: PropTypes.string.isRequired
+};
+
+MenuItem.defaultProps = {
+    price: '20.0'
+};
 
 export default MenuItem;
