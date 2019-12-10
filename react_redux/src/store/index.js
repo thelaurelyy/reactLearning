6ca383ea4a55817441c4af5faffd95f6be7eba29
thelaurelyy <3246997955@qq.com>
2019-12-10 19:31:12 +0800

    【Redux】安装Redux Dev Tools

--- react_redux/src/store/index.js
+++ react_redux/src/store/index.js
@@ -2,7 +2,10 @@ import { createStore } from 'redux'
 import reducer from './reducer'
 
 // 创建数据存储仓库
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 // 暴露出去
 export default store
