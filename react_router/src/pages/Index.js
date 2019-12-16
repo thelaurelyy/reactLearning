842cc7785eb66f2a8e7d9960d7dfb8663bef83fa
thelaurelyy <3246997955@qq.com>
2019-12-16 15:14:16 +0800

    【Router】路由动态传参的简单demo；

--- react_router/src/pages/Index.js
+++ react_router/src/pages/Index.js
@@ -1,15 +1,28 @@
 import React, { Component }  from 'react'
+import { Link } from 'react-router-dom'
 
 class Index extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            list: [
+                { uid: 123, title: 'yy的测试demo-1' },
+                { uid: 456, title: 'yy的测试demo-2' },
+                { uid: 789, title: 'yy的测试demo-3' },
+            ]
         }
     }
     render() {
         return (
-            <h2>JSpang, Index page.</h2>
+            <ul>
+                {
+                    this.state.list.map((item, index) => {
+                        return (
+                            <li key={index}><Link to={'/list/'+item.uid}>{item.title}</Link></li>
+                        )
+                    })
+                }
+            </ul>
         )
     }
 }
