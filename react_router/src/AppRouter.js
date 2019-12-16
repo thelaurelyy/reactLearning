73e105b9f2ac9f81e64782510e722f52d3803db7
thelaurelyy <3246997955@qq.com>
2019-12-16 14:56:38 +0800

    【Router】（1）将无状态组件改写为UI组件；（2）路由动态传递参数；

--- react_router/src/AppRouter.js
+++ react_router/src/AppRouter.js
@@ -1,31 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Index from "./pages/Index";
+import List from "./pages/List";
 
-function Index() {
-    return (
-        <h2>JSpang, Index page.</h2>
-    )
-}
+class AppRouter extends Component{
 
-function List() {
-    return (
-        <h2>Myself, List page.</h2>
-    )
-}
-
-function AppRouter() {
-    return (
-        <div>
-            <Router>
-                <ul>
-                    <li><Link to='/'>首页</Link></li>
-                    <li><Link to='/list/'>列表</Link></li>
-                </ul>
-                <Route path='/' component={Index} exact />
-                <Route path='/list/' component={List} />
-            </Router>
-        </div>
-    )
+    render() {
+        return (
+            <div>
+                <Router>
+                    <ul>
+                        <li><Link to='/'>首页</Link></li>
+                        <li><Link to='/list/123'>列表</Link></li>
+                    </ul>
+                    <Route path='/' component={Index} exact />
+                    <Route path='/list/:id' component={List} />
+                </Router>
+            </div>
+        )
+    }
 }
 
 export default AppRouter;
---
+++ react_router/src/pages/Index.js
@@ -0,0 +1,17 @@
+import React, { Component }  from 'react'
+
+class Index extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <h2>JSpang, Index page.</h2>
+        )
+    }
+}
+
+export default Index
