5f886c00b1e798efefd2c25b236614b2b30445eb
thelaurelyy <3246997955@qq.com>
2019-12-18 16:10:38 +0800

    【Redux-复习】配置redux-saga

--- react_redux/src/TodoList.js
+++ react_redux/src/TodoList.js
@@ -10,7 +10,7 @@ import { changeInputAction, addItemAction, deleteItemAction/*, getListAction*/, 
 class TodoList extends Component {
     constructor(props) {
         super(props);
-        // store中的数据不要直接使用在UI层  
+        // store中的数据不要直接使用在UI层
         // 可以直接复制给组件的state
         this.state = store.getState();
         this.changeInputValue = this.changeInputValue.bind(this);
@@ -20,18 +20,19 @@ class TodoList extends Component {
 
         store.subscribe(this.storeChange) // 订阅Redux的状态
     }
-    componentDidMount() {
-        // axios.get('https://www.easy-mock.com/mock/5cfcce489dc7c36bd6da2c99/xiaojiejie/getList').then((res)=>{
-        //     console.log(res);
-        //     const action = getListAction(res.data);
-        //     store.dispatch(action)
-        // })
-
-        // use redux-thunk handle axios request
-        const action = getTodoList(); // 这里的action是一个函数
-        console.log(action)
-        store.dispatch(action);
-    }
+    // use redux-saga instead of redux-thunk
+    // componentDidMount() {
+    //     // axios.get('https://www.easy-mock.com/mock/5cfcce489dc7c36bd6da2c99/xiaojiejie/getList').then((res)=>{
+    //     //     console.log(res);
+    //     //     const action = getListAction(res.data);
+    //     //     store.dispatch(action)
+    //     // })
+    //
+    //     // use redux-thunk handle axios request
+    //     const action = getTodoList(); // 这里的action是一个函数
+    //     console.log(action)
+    //     store.dispatch(action);
+    // }
     render() {
         // UI组件
         // return (
--- react_redux/src/store/index.js
+++ react_redux/src/store/index.js
@@ -1,11 +1,24 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga'
 import reducer from './reducer'
+import mySagas from './sagas'
 
+
+// 创建增强器
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 
-const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+// redux-saga 中间件
+const sagaMiddleware = createSagaMiddleware();
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware ));
+sagaMiddleware.run(mySagas);
+
+
+// redux-thunk 中间件
+// const enhancer = composeEnhancers(applyMiddleware(thunk));
+
 
 // 创建数据存储仓库
 const store = createStore(
@@ -14,6 +27,7 @@ const store = createStore(
     // applyMiddleware(thunk),
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
 
 // 暴露出去
 export default store
