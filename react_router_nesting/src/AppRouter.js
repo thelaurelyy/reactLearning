d2dbe50b4d753e46053963465d36e74daf24d461
thelaurelyy <3246997955@qq.com>
2019-12-18 11:53:59 +0800

    【Router Nesting】编写嵌套路由demo实例2

--- react_router_nesting/src/AppRouter.js
+++ react_router_nesting/src/AppRouter.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link  } from "react-router-dom";
 import Index from './Pages/Index'
 import Video from './Pages/Video'
+import WorkPlace from "./Pages/WorkPlace";
 import './index.css'
 
 function AppRouter() {
@@ -13,13 +14,14 @@ function AppRouter() {
                     <ul>
                         <li> <Link to="/">博客首页</Link> </li>
                         <li><Link to="/video/">视频教程</Link> </li>
-                        <li><Link to="">职场技能</Link> </li>
+                        <li><Link to="/workplace/">职场技能</Link> </li>
                     </ul>
                 </div>
 
                 <div className="rightMain">
                     <Route path="/" component={Index} exact />
                     <Route path="/video/" component={Video} />
+                    <Route path="/workplace/" component={WorkPlace} />
                 </div>
             </div>
         </Router>
---
+++ react_router_nesting/src/Pages/workPlace/Money.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Money() {
+    return (
+        <h2>this is Money Page.</h2>
+    )
+}
+
+export default Money
