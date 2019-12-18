797721839f176e309f7d4015303deec96f05b56f
thelaurelyy <3246997955@qq.com>
2019-12-18 11:36:35 +0800

    【Router Nesting】编写嵌套路由demo实例

---
+++ react_router_nesting/src/Pages/video/Flutter.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Flutter() {
+    return (
+        <h2>我是Flutter 页面.</h2>
+    )
+}
+export default Flutter
---
+++ react_router_nesting/src/Pages/video/ReactPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ReactPage() {
+    return (
+        <h2>我是React 页面.</h2>
+    )
+}
+export default ReactPage
