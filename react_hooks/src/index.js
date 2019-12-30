d3705bd0e6ef634a791d294df1d07a7981a85f10
thelaurelyy <3246997955@qq.com>
2019-12-30 16:20:53 +0800

    利用useReucer在浏览器中实现计数器的加减

--- react_hooks/src/index.js
+++ react_hooks/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import Example from './Example';
 // import Example from "./ExampleHooks";
-import Example from './ExampleContext'
+// import Example from './ExampleContext'
+import Example from './ExampleReducer'
 
 ReactDOM.render( <Example />, document.getElementById('root'));
 
