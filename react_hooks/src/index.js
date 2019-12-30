eb24af5d1cece389010dfb1b92f8457e3a633df9
thelaurelyy <3246997955@qq.com>
2019-12-30 16:05:23 +0800

    useContext进行父子组件传值

--- react_hooks/src/index.js
+++ react_hooks/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import Example from './Example';
-import Example from "./ExampleHooks";
+// import Example from "./ExampleHooks";
+import Example from './ExampleContext'
 
 ReactDOM.render( <Example />, document.getElementById('root'));
 
