981df7c1d5abad4a71ce106055711eb59a5cec20
thelaurelyy <3246997955@qq.com>
2019-12-31 11:28:09 +0800

    给函数设置别名（小实验）

--- react_hooks/src/example_replace-redux/ShowArea.js
+++ react_hooks/src/example_replace-redux/ShowArea.js
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
-import { ColorContext, UPDATE_COLOR } from "./color";
+import { UPDATE_COLOR } from "./color";
+import { ColorContext as aa } from './color'
 
 function ShowArea() {
-    const { dispatch } = useContext(ColorContext)
+    const { dispatch } = useContext(aa)
     return (
         <div>
             <button onClick={ ()=>{dispatch({type:UPDATE_COLOR, color: 'purple'})} }>紫色</button>
