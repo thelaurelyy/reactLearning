4c87d8b5504cb9d698836bdaf9d7a981372d27c2
thelaurelyy <3246997955@qq.com>
2020-02-02 19:17:13 +0800

    路由的标签跳转Link和编程式跳转Router.push()

---
+++ （9）next-create/pages/jspangA.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default () => (
+    <>
+        <div>jspang-A页面</div>
+        <div><Link href='/'><a>返回首页</a></Link></div>
+    </>
+)
